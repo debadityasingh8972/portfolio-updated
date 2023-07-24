9e798af6a4f08bbe4118e36494ca0b9f0d784c2b
Debaditya <debadityasingh9@gmail.com>
2023-07-25 01:19:24 +0530

    Complete project updated

---
+++ src/data/WorkData.js
@@ -0,0 +1,58 @@
+export const Work = [
+    {
+        id:1,
+        name:"Cyber-fiction Landing Page",
+        description:"It is build with JavaScript, with styledComponents and GSAP & Scrolltrigger plugin for smooth scrolling animations.",
+        tags:["JavaScript","gsap","styled-Components", "locomotive-scroll"],
+        
+        demo:"https://cyber-fiction-two.vercel.app/",
+        github:"https://github.com/debadityasingh8972/Cyber-fiction"
+    },
+    {
+        id:2,
+        name:"Newton's Block",
+        description:"A Web Game, built using JavaScript. You have to stack a tower block as high as you can",
+        tags:["JavaScript","css", "HTML"],
+        
+        demo:"https://newtons-block-ds4.netlify.app/",
+        github:"https://github.com/debadityasingh8972/Newton-s-Block"
+        
+    },
+    {
+        id:3,
+        name:"NFT Marketplace",
+        description:"A FullStack NFT Marketplace designed using NextJS. With image storage for NFT using infura API. Created Smart contract using solidity on hardhat environment",
+        tags:["solidity","hardhat","nextJs","infura-api","metamask"],
+        
+        demo:"https://nft-marketplace-psi-one.vercel.app/",
+        github:"https://github.com/debadityasingh8972/nft-marketplace-demo"
+    },
+    {
+        id:4,
+        name:"Three Graces Design",
+        description:"Animated webpage on Antonio Canova's legendary Three Graces, built using ThreeJs and framerMotion. It has really smooth animations.",
+        tags:["threeJs","styledComponents","webgl", "vanillaJs"],
+        
+        demo:"https://threejs-graces-ds3.netlify.app/",
+        github:"https://github.com/debadityasingh8972/graces-threejs-project"
+    },{
+        id:5,
+        name:"2048 Game - Clone",
+        description:"2048 is a block game build using react and algorithmic logic",
+        tags:["react","scss","framer-motion"],
+        
+        demo:"https://2048-ds3.netlify.app/",
+        github:"https://github.com/debadityasingh8972/2048-game-animated"
+        
+    },
+    {
+        id:6,
+        name:"Simple Animated Card",
+        description:"A cool looking animated Debit card design. It was one of my first projects. Built from scratch.",
+        tags:["HTML","css"],
+        
+        demo:"https://flipping-card-ui.netlify.app/",
+        github:"https://github.com/debadityasingh8972/Mini-projects/tree/main/Animated%20Credit%20Card%20UI"
+        
+    },
+] 
